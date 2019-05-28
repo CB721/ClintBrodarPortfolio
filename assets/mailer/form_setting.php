1ddb6f0a3545ed1194fdc0036be430cbe6875d05
CB721 <clintbrodar@gmail.com>
2019-05-28 07:58:08 -0700

    Mailing contact form function

---
+++ assets/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Profile Website'"; //letter subject
+	$to = 'clintbrodar@gmail.com'; // Enter Your E-mail
+	$from = 'admin@clintbrodar.com'; // Admin e-mail
+	$fromName = 'Clint Brodar'; // Your company name
+	$charset = 'UTF-8';
+?>
