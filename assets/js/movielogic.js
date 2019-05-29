e05aa2b5cdd8d37f99b6f33bd5dc1db95cec01df
CB721 <clintbrodar@gmail.com>
2019-05-28 22:39:23 -0700

    Time difference configured

--- assets/js/movielogic.js
+++ assets/js/movielogic.js
@@ -1,4 +1,11 @@
-//every year, add certain amount to views of star wars
+//every year, add to views of star wars
+    
+    //variable for current date
+    var currentTime = moment().format("DD, MMM, YYYY");
+    //variable for may 4th 2019
+    var originialTime = "04, May, 2019";
+
+    console.log(currentTime.diff(originialTime, 'days'));
     //update every may 4th
     //add 20 to hours total
     //push to html
