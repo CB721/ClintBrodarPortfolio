d97d8de7214c86164b15d366a791e1ab886a41a1
CB721 <clintbrodar@gmail.com>
2019-05-29 13:05:16 -0700

    Additional psuedo code

--- assets/js/movielogic.js
+++ assets/js/movielogic.js
@@ -1,11 +1,24 @@
 //every year, add to views of star wars
     
     //variable for current date
-    var currentTime = moment().format("DD, MMM, YYYY");
+    var currentDayMonth = moment().format("DD, MMM, YYYY");
+    //variable for current year
+    var currentYear = moment().format("YYYY");
     //variable for may 4th 2019
-    var originialTime = "04, May, 2019";
+    var originialTime = "04, May, 2018";
+    //variable for hours
+    var starWarsHours = 233;
 
-    console.log(currentTime.diff(originialTime, 'days'));
+    // Difference between the times
+    var timeDifference = moment().diff(moment(originialTime), 'years');
+    console.log(timeDifference);
+
+    if (timeDifference === 1) {
+        starWarsHours + 20;
+        console.log(starWarsHours);
+    }
+
     //update every may 4th
     //add 20 to hours total
     //push to html
+    $("#starWarsHours").prepend(starWarsHours + " ");
