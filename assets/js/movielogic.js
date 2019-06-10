dadf4a146f64caf33f7ad15ef274968860e5fe37
CB721 <clintbrodar@gmail.com>
2019-06-10 12:39:30 -0700

    Cleaned up code

--- assets/js/movielogic.js
+++ assets/js/movielogic.js
@@ -3,15 +3,13 @@
     var currentDayMonth = moment().format("DD, MMM, YYYY");
     //variable for current year
     var currentYear = moment().format("YYYY");
-    //variable for may 4th 2019
+    //set original time back one year for reference
     var originialTime = "04, May, 2018";
     //variable for hours from inital tally
     var starWarsHours = 233;
     //variable for yearly hour additions
     var yearHours = 20;
-    // current hours
-    var currentHours;
-    // Difference between the times
+    //difference between the times in years
     var timeDifference = moment().diff(moment(originialTime), 'years');
     //for every additional year, add 20 hours
     for (var i = 0; i < timeDifference; i ++) {
